78a8acf88fa8d0732716e0cfa338321f09d73ff4
hanseulhee <3021062@gmail.com>
2021-01-25 19:29:29 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const open = document.querySelector('.python');
+const open1 = document.querySelector('.likelion');
+const open2 = document.querySelector('.DB');
+const open3 = document.querySelector('.Java');
+
+// Toggle nav
+open.addEventListener('click', () =>
+  document.body.classList.toggle('show-nav')
+);
+
+open1.addEventListener('click', () =>
+  document.body.classList.toggle('stella')
+);
+
+open2.addEventListener('click', () =>
+  document.body.classList.toggle('show-nav')
+);
+
+open3.addEventListener('click', () =>
+  document.body.classList.toggle('show-nav')
+);
